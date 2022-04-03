081dbe23c87394a98f7643998eef5273ba714564
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2022-04-03 21:09:42 +0300

    refactor is session

--- src/test/java/tests/sample/AndroidSampleTests.java
+++ src/test/java/tests/sample/AndroidSampleTests.java
@@ -24,7 +24,7 @@ public class AndroidSampleTests {
         caps.setCapability("browserstack.key", App.config.browserstackKey());
 
         // Set URL of the application under test
-        caps.setCapability("app", App.config.browserstackSessionId());
+        caps.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
 
         // Specify device and os_version for testing
         caps.setCapability("device", "Google Pixel 3");
